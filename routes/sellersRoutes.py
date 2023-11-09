3d1896f6c42d3c90da576979119e0dab72a36e78
unknown <matheus.fcarvalho2001@gmail.com>
2023-11-09 15:45:11 -0300

    vendedorlocked

--- routes/sellersRoutes.py
+++ routes/sellersRoutes.py
@@ -27,13 +27,12 @@ from pymongo import MongoClient
 bp = Blueprint('sellers', __name__)
 MONGO_URI = "mongodb+srv://matheusfcarvalho2001:3648@cluster0.rioem39.mongodb.net/?retryWrites=true&w=majority"
 
-@bp.route('/<nome>', methods=['GET'])
+@bp.route('/<nome>', methods=['GET', 'POST'])
 def perfil_vendedor(nome):
     # Conectar ao banco de dados MongoDB
     client = MongoClient(MONGO_URI)
     db = client['pequi']
     vendedores_collection = db['vendedores']
-
     # Obter o documento do banco de dados
     documento = vendedores_collection.find_one({})
 
@@ -49,26 +48,46 @@ def perfil_vendedor(nome):
         clientesDbOfSeller = {cliente: clientesDb.get(cliente) for cliente in clientes}
         
         # Passe os dados como um dicionário com as chaves correspondentes
-        dados_vendedor = {
-            "nome": nome,
-            "totalVendido": totalVendidoFormatado,
-            "qtdVendas": vendedor.get("qtdVendas", 0),
-            "qtdPortas": vendedor.get("qtdPortas", 0),
-            "qtdClientes": qtdClient,
-            "clientesName": clientes, 
-            "clientesDb": clientesDbOfSeller,
-            "rank_vendedor": vendedor.get("rank_vendedor", 0),
-            "rank_porteiro": vendedor.get("rank_porteiro", 0),
-            "rank_value": vendedor.get("rank_value", 0),
-            "rank_cliente": vendedor.get("rank_cliente", 0),
+        senhaSystem = {
+            'michelle boschetti':'golden',
+            'beatriz boschetti':'senhora',
+            'bianca boschetti':'',
+            'fernando boschetti':'golf',
+            'valter souza':'esfola',
+            'willian souza':'sistema', 
+            'luciana rocha':'fua',
+            'tatiane brockyeld':'cocacola',
 
         }
 
+        senha = request.form.get('senha')
+        if request.method == 'POST':
+            if senha == senhaSystem.get(nome):
+                dados_vendedor = {
+                    "nome": nome,
+                    "senhaSystem": senhaSystem,
+                    "totalVendido": totalVendidoFormatado,
+                    "qtdVendas": vendedor.get("qtdVendas", 0),
+                    "qtdPortas": vendedor.get("qtdPortas", 0),
+                    "qtdClientes": qtdClient,
+                    "clientesName": clientes, 
+                    "clientesDb": clientesDbOfSeller,
+                    "rank_vendedor": vendedor.get("rank_vendedor", 0),
+                    "rank_porteiro": vendedor.get("rank_porteiro", 0),
+                    "rank_value": vendedor.get("rank_value", 0),
+                    "rank_cliente": vendedor.get("rank_cliente", 0),
+                    "senha": senha  # Passa a senha para o template
+                }
+            else:
+                # Se a senha estiver incorreta, passe apenas o nome e a senha
+                dados_vendedor = {"nome": nome, "senha": "", "acesso_negado": True, "senhaSystem": senhaSystem}
+        else:
+            # Se não for uma solicitação POST, passe apenas o nom
+            dados_vendedor = {"nome": nome, "senha": "","senhaSystem": senhaSystem }
         return render_template('perfil_vendedor.html', **dados_vendedor)
     else:
         # Se o vendedor não for encontrado, você pode lidar com isso de acordo com sua lógica, como exibir uma mensagem de erro.
         return "Vendedor não encontrado", 404
-
 
 @bp.route('/admin', methods=['GET'])
 def admin():
@@ -107,7 +126,6 @@ def admin():
                     "rank_value": vendedor.get("rank_value", 0),
                     "rank_cliente": vendedor.get("rank_cliente", 0),
                 }
-
                 # Adicione o dicionário de dados_vendedor à lista de dados_vendedores
                 dados_vendedores.append(dados_vendedor)
     dados_vendedores = sorted(dados_vendedores, key=lambda x: x['rank_value'], reverse=False)[1:]
